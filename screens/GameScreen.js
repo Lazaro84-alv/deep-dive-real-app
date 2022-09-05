338e86e30f16e736278056b3785768313c6c1a70
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-09-05 17:23:02 -0300

    Introducing the Dimensions API

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -102,7 +102,8 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 24
+    padding: 24,
+    alignItems: 'center'
   },
   InstructionText: {
     marginBottom: 12
